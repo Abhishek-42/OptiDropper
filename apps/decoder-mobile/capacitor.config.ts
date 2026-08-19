fce51cf7308c452e4cbe432480bfae69b7b4cc84
Abhishek <abhishek2005abhi@gmail.com>
2026-08-20 04:05:49 +0530

    apk cofig bug fix 6

--- apps/decoder-mobile/capacitor.config.ts
+++ apps/decoder-mobile/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 's2c.decoder.app',
-  appName: 'S2C Decoder',
+  appId: 'com.optidropper.app',
+  appName: 'Optidropper',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
